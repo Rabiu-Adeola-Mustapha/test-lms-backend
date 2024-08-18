270ff413bf5476113400b14c917eb836f9d367ae
Mustapaha Rabiu <konnect1795@gmail.com>
2024-08-18 10:00:54 +0100

    try36

--- src/common/mail/mail.module.ts
+++ src/common/mail/mail.module.ts
@@ -1,28 +1,46 @@
 /* eslint-disable prettier/prettier */
+require("dotenv").config();
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service';
 
 @Module({
-    imports : [
-        MailerModule.forRootAsync({
-            imports: [ConfigModule], // Import ConfigModule to use ConfigService
-            useFactory: async (configService: ConfigService) => ({
-                transport: {
-                    host: configService.get<string>('MAIL_HOST'),
-                    port: 465,
-                    secure: true,
-                    auth: {
-                        user: configService.get<string>('MAIL_USER'),
-                        pass: configService.get<string>('MAIL_PASS'),
-                    },
-                },
-            }),
-            inject: [ConfigService], // Inject ConfigService to use in the factory function
-        }),
-    ],
-    providers: [MailService],
-    exports: [MailService]
+  imports: [
+    // MailerModule.forRootAsync({
+    //     imports: [ConfigModule], // Import ConfigModule to use ConfigService
+    //     useFactory: async (configService: ConfigService) => ({
+    //         transport: {
+    //             host: configService.get<string>('MAIL_HOST'),
+    //             port: 465,
+    //             secure: true,
+    //             auth: {
+    //                 user: configService.get<string>('MAIL_USER'),
+    //                 pass: configService.get<string>('MAIL_PASS'),
+    //             },
+    //         },
+    //     }),
+    //     inject: [ConfigService], // Inject ConfigService to use in the factory function
+    // }),
+
+    MailerModule.forRootAsync({
+      useFactory: async () => ({
+        transport: {
+          host: process.env.MAIL_HOST,
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+          },
+        },
+        defaults: {
+          from: '"Mcanderson Institute" support@mivant.co', // Make sure this domain is correctly configured
+        },
+      }),
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
